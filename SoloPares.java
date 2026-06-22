c45add890bf17c784dfa959a362ccc3f18dbc93e
jpg-ms-yt <tinovalegomez@gmail.com>
2026-06-22 10:01:54 -0500

    Add files via upload

---
+++ SoloPares.java
@@ -0,0 +1,12 @@
+public class SoloPares {
+    public static void main(String[] args) {
+        System.out.println("Números pares del 1 al 100:");
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                continue; // Si es impar, salta a la siguiente vuelta
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
